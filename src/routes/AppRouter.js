0e4e6399e07d5b10f613ad4f139168fddb24a0b6
Victor Bolaños Solís <viktorb.132@gmail.com>
2021-07-08 18:43:11 -0500

    Routes protected

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
     BrowserRouter as Router,
-    Switch,
-    Route
+    Switch
 } from "react-router-dom";
+import { AuthContext } from '../auth/AuthContext';
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
+
+    const { user: { logged } } = useContext(AuthContext)
     return (
         <Router>
             <div>
                 <Switch>
-                    <Route exact path="/login" component={ LoginScreen } />
-                    <Route path="/" component={ DashboardRoutes } />
+                    <PublicRoute exact path="/login" component={ LoginScreen } isNotAuthenticated={ !logged } />
+                    <PrivateRoute path="/" component={ DashboardRoutes } isAuthenticated= { logged } />
                 </Switch>
             </div>
         </Router>
